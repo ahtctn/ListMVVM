6f9b7c6ac971d58faed84517f7b13aac07081d87
AhmetAliCetin <ahtctn@gmail.com>
2022-08-23 16:10:37 +0300

    Gerçek olmayan bir numaralandırma gerçekleştirildi.

--- MVVM4List-2/ModelViews/ContactViewModel.swift
+++ MVVM4List-2/ModelViews/ContactViewModel.swift
@@ -21,22 +21,22 @@ class ContactViewModel: ObservableObject {
     }
     
     var peopleData = [
-        ContactDataModel(ad: "Ahmet Ali Çetin", telNo: "534-613-2764", email: "ahtctn@gmail.com"),
-        ContactDataModel(ad: "Erkan Sevim", telNo: "530-118-7826", email: "erkansevm@gmail.com"),
-        ContactDataModel(ad: "Yunus Emre Keskin", telNo: "542-785-0792", email: "yek@gmail.com"),
-        ContactDataModel(ad: "Erdem Sarıkaya", telNo: "554-613-2764", email: "erdemsarkya@gmail.com"),
-        ContactDataModel(ad: "Erbil Necip Gümüş", telNo: "506-708-3583", email: "eng@engkimya.com"),
-        ContactDataModel(ad: "Mehmet Salihoğlu", telNo: "542-789-1452", email: "msalihoglu@univera.com"),
-        ContactDataModel(ad: "Sami Delirmez", telNo: "546-755-1792", email: "sami@gmail.com"),
-        ContactDataModel(ad: "Nevin Tuğba Serçe", telNo: "556-567-8765", email: "nevintugbaserce@gmail.com"),
-        ContactDataModel(ad: "Demir Toprak", telNo: "543-432-2354" , email: "demirtoprak@outlook.com"),
-        ContactDataModel(ad: "Salih Sedir", telNo: "554-456-7684", email: "sedirsalih@hotmail.com"),
-        ContactDataModel(ad: "Tolga Aydın", telNo: "564-234-7890", email: "tolgaaydin@normholding.com"),
-        ContactDataModel(ad: "Melih Can Macit", telNo: "567-234-3463", email: "melihcanmacit@outlook.com"),
-        ContactDataModel(ad: "Esma Sürücü", telNo: "543-789-5335", email: "surucu@hotmail.com"),
-        ContactDataModel(ad: "Ahmet Çerkez", telNo: "542-546-2341", email: "ahmetcerkez@gmail.com"),
-        ContactDataModel(ad: "Didem Sarıkaya", telNo: "567-789-0987", email: "didemsarikaya@hotmail.com"),
-        ContactDataModel(ad: "Ahmet Yılmaz", telNo: "556-678-3456", email: "ahmetyilmaz@gmail.com")
+        ContactDataModel(ad: "Ahmet Ali Çetin", telNo: "555-555-5555", email: "ahtctn@gmail.com"),
+        ContactDataModel(ad: "Erkan Sevim", telNo: "555-555-5555", email: "erkansevm@gmail.com"),
+        ContactDataModel(ad: "Yunus Emre Keskin", telNo: "555-555-5555", email: "yek@gmail.com"),
+        ContactDataModel(ad: "Erdem Sarıkaya", telNo: "555-555-5555", email: "erdemsarkya@gmail.com"),
+        ContactDataModel(ad: "Erbil Necip Gümüş", telNo: "555-555-5555", email: "eng@engkimya.com"),
+        ContactDataModel(ad: "Mehmet Salihoğlu", telNo: "555-555-5555", email: "msalihoglu@univera.com"),
+        ContactDataModel(ad: "Sami Delirmez", telNo: "555-555-5555", email: "sami@gmail.com"),
+        ContactDataModel(ad: "Nevin Tuğba Serçe", telNo: "555-555-5555", email: "nevintugbaserce@gmail.com"),
+        ContactDataModel(ad: "Demir Toprak", telNo: "555-555-5555" , email: "demirtoprak@outlook.com"),
+        ContactDataModel(ad: "Salih Sedir", telNo: "555-555-5555", email: "sedirsalih@hotmail.com"),
+        ContactDataModel(ad: "Tolga Aydın", telNo: "555-555-5555", email: "tolgaaydin@normholding.com"),
+        ContactDataModel(ad: "Melih Can Macit", telNo: "555-555-5555", email: "melihcanmacit@outlook.com"),
+        ContactDataModel(ad: "Esma Sürücü", telNo: "555-555-5555", email: "surucu@hotmail.com"),
+        ContactDataModel(ad: "Ahmet Çerkez", telNo: "555-555-5555", email: "ahmetcerkez@gmail.com"),
+        ContactDataModel(ad: "Didem Sarıkaya", telNo: "555-555-5555", email: "didemsarikaya@hotmail.com"),
+        ContactDataModel(ad: "Ahmet Yılmaz", telNo: "555-555-5555", email: "ahmetyilmaz@gmail.com")
     ]
 }
 
